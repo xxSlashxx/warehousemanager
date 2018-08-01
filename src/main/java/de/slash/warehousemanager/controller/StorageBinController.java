588efbb36c54b9769865eaa509a806bba3c77e4d
xxSlashxx <roland.speckmeier@gmx.de>
2018-08-01 20:50:12 +0200

    Added 'create'- and 'index'-Functionality for StorageBins

--- src/main/java/de/slash/warehousemanager/controller/StorageBinController.java
+++ src/main/java/de/slash/warehousemanager/controller/StorageBinController.java
@@ -1,10 +1,32 @@
 package de.slash.warehousemanager.controller;
 
+import de.slash.warehousemanager.entity.StorageBin;
+import de.slash.warehousemanager.entity.Warehouse;
+import de.slash.warehousemanager.service.StorageBinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/storagebins")
 public class StorageBinController
 {
+    @Autowired
+    private StorageBinService storageBinService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<StorageBin> index()
+    {
+        return storageBinService.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public StorageBin createStorageBin(@RequestBody StorageBin storageBin)
+    {
+        return storageBinService.createStorageBin(storageBin);
+    }
 }
